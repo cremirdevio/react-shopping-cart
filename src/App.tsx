2a239d22a615515b14b8fdde50724d0efd262bc6
cremirdevio <josephajibodu@gmail.com>
2021-12-16 22:23:19 +0100

    Items can now be added to cart

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { AddShoppingCart } from "@material-ui/icons";
 // Styles
 import { StyledButton, Wrapper } from "./App.styles";
 import Item from "./Item/Item";
+import Cart from "./Cart/Cart";
 // Types
 export type CartItemType = {
   id: number;
@@ -32,7 +33,21 @@ const App = () => {
   const getTotalItems = (items: CartItemType[]) => {
     return items.reduce((ack: number, item) => ack + item.amount, 0);
   };
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems((prev) => {
+      // Is the item already added in the cart?
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+      if (isItemInCart) {
+        return prev.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
+      // First time the item is added
+      return [...prev, { ...clickedItem, amount: 1 }];
+    });
+  };
   const handleRemoveFromCart = () => null;
 
   if (isLoading) return <LinearProgress />;
@@ -41,7 +56,11 @@ const App = () => {
   return (
     <Wrapper>
       <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
-        Cart goes here
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
       </Drawer>
       <StyledButton onClick={() => setCartOpen(true)}>
         <Badge badgeContent={getTotalItems(cartItems)} color="error">
--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -1,3 +1,30 @@
-const Cart = () => {};
+import CartItem from "../CartItem/CartItem";
+// Types
+import { CartItemType } from "../App";
+// Styles
+import { Wrapper } from "../App.styles";
+
+type Props = {
+  cartItems: CartItemType[];
+  addToCart: (clickedItem: CartItemType) => void;
+  removeFromCart: (id: number) => void;
+};
+
+const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+  return (
+    <Wrapper>
+      <h2>Your shopping cart</h2>
+      {!cartItems.length ? <p>No Items in Cart</p> : null}
+      {cartItems.map((item) => (
+        <CartItem
+          key={item.id}
+          item={item}
+          addToCart={addToCart}
+          removeFromCart={removeFromCart}
+        />
+      ))}
+    </Wrapper>
+  );
+};
 
 export default Cart;
